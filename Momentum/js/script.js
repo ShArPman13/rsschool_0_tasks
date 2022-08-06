78bfe6de5f7f900b4f7c186c6fbf13141852c48a
sharpman13 <sharpman2001@gmail.com>
2022-08-06 14:45:20 +0200

    feat: add translation, picsapi, start settings

--- Momentum/js/script.js
+++ Momentum/js/script.js
@@ -9,7 +9,67 @@ const greetings = document.querySelector('.greeting');
 const hours = date.getHours();
 
 const nameSharp = document.querySelector('.name');
+
 const localSettings = {};
+
+//------------------------------TRANSLATE--------------------------------------------
+
+const greetingTranslation = {
+  en: ['Good Morning', 'Good Afternoon', 'Good Evening', 'Good Night'],
+  pl: ['Dzień dobry', 'Dzień dobry', 'Dobry wieczór', 'Dobranoc'],
+  ru: ['Доброе утро', 'Добрый день', 'Добрый вечер', 'Доброй ночи']
+}
+
+const placeHolderNameTranslation = ['Enter name', 'Wpisz imię', 'Введите имя'];
+
+function showPlaceHolder() {
+  nameSharp.placeholder = placeHolderNameTranslation[i];
+}
+
+function showGreeting(lang = 'en') {
+  let getTime = getTimeOfDay();
+  let count;
+  switch (getTime) {
+    case 'morning':
+      count = 0;
+      break;
+    case 'afternoon':
+      count = 1;
+      break;
+    case 'evening':
+      count = 2;
+      break;
+    case 'night':
+      count = 3;
+      break;
+  }
+  greetings.textContent = greetingTranslation[lang][count];
+  showPlaceHolder();
+}
+
+const languageBtn = document.querySelector('.language');
+languageBtn.textContent = 'en';
+
+const arrLang = ['en', 'pl', 'ru'];
+let i = 0;
+let quotesSwitch = '';
+
+languageBtn.addEventListener('click', () => {
+  i++;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=${arrLang[i]}&appid=a7d6cf5c06b60ebd06921df6a6b2725a&units=metric`;
+  if (i === 3) { i = 0 };
+  languageBtn.textContent = arrLang[i];
+  showGreeting(arrLang[i]);
+  getWeather(url);
+  if (i === 2) {
+    quotesSwitch = 'rus'
+    getQuotesRus();
+  }
+  else {
+    quotesSwitch = 'eng'
+    getQuotes();
+  }
+});
 
 function showTime() {
   const date = new Date();
@@ -17,12 +77,12 @@ function showTime() {
   time.textContent = currentTime;
   showDate(date);
   setTimeout(showTime, 1000);
-  greetings.textContent = `Good ${getTimeOfDay()}`;
+  showGreeting(arrLang[i]);
 }
 showTime();
 
 function showDate(date) {
-  const currentDate = date.toLocaleDateString('en-US', options);
+  const currentDate = date.toLocaleDateString(arrLang[i], options);
   myDate.textContent = currentDate;
 }
 
@@ -58,6 +118,9 @@ const slideNext = document.querySelector('.slide-next')
 const slidePrev = document.querySelector('.slide-prev')
 let bgNum = getRandomNum();
 
+const arrOfPhotoBank = document.querySelectorAll('.bibl-photo');
+let globalActiveTag = getTimeOfDay();
+
 function setBg() {
   const img = new Image();
   let timeOfDay = getTimeOfDay();
@@ -69,23 +132,39 @@ function setBg() {
 setBg()
 
 function getSlideNext() {
-  const num = +bgNum + 1;
-  bgNum = fixNum(num);
-  if (bgNum === 21) {
-    bgNum = '01';
+  if (arrOfPhotoBank[0].classList.contains('active')) {
+    const num = +bgNum + 1;
+    bgNum = fixNum(num);
+    if (bgNum === 21) {
+      bgNum = '01';
+    }
+    setBg(bgNum);
   }
-  setBg(bgNum);
+  else if (arrOfPhotoBank[1].classList.contains('active')) {
+    getLinkToImageFromUnsplash();
+  }
+  else {
+    getLinkToImageFromFlickr();
+  }
 }
 
 slideNext.addEventListener('click', getSlideNext);
 
 function getSlidePrev() {
-  const num = +bgNum - 1;
-  bgNum = fixNum(num);
-  if (bgNum === '00') {
-    bgNum = '20';
+  if (arrOfPhotoBank[0].classList.contains('active')) {
+    const num = +bgNum - 1;
+    bgNum = fixNum(num);
+    if (bgNum === '00') {
+      bgNum = '20';
+    }
+    setBg(bgNum);
   }
-  setBg(bgNum);
+  else if (arrOfPhotoBank[1].classList.contains('active')) {
+    getLinkToImageFromUnsplash();
+  }
+  else {
+    getLinkToImageFromFlickr();
+  }
 }
 
 slidePrev.addEventListener('click', getSlidePrev);
@@ -110,7 +189,7 @@ async function getWeather(url) {
     temperature.textContent = `${Math.round(data.main.temp)}°C`;
     weatherDescription.textContent = data.weather[0].description;
     windSpeed.textContent = `Wind Speed: ${data.wind.speed}`;
-    humidity.textContent = `Humidity: ${data.main.humidity}`
+    humidity.textContent = `Humidity: ${data.main.humidity}`;
   } catch (error) {
     weatherIcon.className = 'weather-icon owf';
     temperature.textContent = '';
@@ -121,7 +200,7 @@ async function getWeather(url) {
 }
 
 city.addEventListener('change', () => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=en&appid=a7d6cf5c06b60ebd06921df6a6b2725a&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=${arrLang[i]}&appid=a7d6cf5c06b60ebd06921df6a6b2725a&units=metric`;
   localSettings.url = url;
   getWeather(url);
   let cityNameFirstLetterBig = city.value.toString();
@@ -168,13 +247,30 @@ async function getQuotes() {
   const quotes = 'assets/quotes.json';
   const res = await fetch(quotes);
   const data = await res.json();
-  let randomQuote = Math.floor(Math.random() * 101)
-  quote.textContent = `" ${data[randomQuote].quote} "`;
+  let randomQuote = Math.floor(Math.random() * 1623)
+  quote.textContent = `" ${data[randomQuote].text} "`;
   author.textContent = `- ${data[randomQuote].author} -`;
 }
+
 getQuotes();
 
-changeQuote.addEventListener('click', getQuotes);
+async function getQuotesRus() {
+  const quotesRus = 'assets/quotes_rus.json'
+  const resRus = await fetch(quotesRus);
+  const dataRus = await resRus.json();
+  let randomQuoteRus = Math.floor(Math.random() * 479)
+  quote.textContent = `" ${dataRus[randomQuoteRus]} "`;
+  author.textContent = `- Программист под номером ${randomQuoteRus} -`;
+}
+
+changeQuote.addEventListener('click', () => {
+  if (quotesSwitch == 'rus') {
+    getQuotesRus();
+  }
+  else {
+    getQuotes();
+  }
+});
 
 //------------------------------AUDIO--------------------------------------------
 
@@ -187,8 +283,14 @@ const audio = new Audio(playList[playNum].src);
 let playListContainer = document.querySelector('.play-list');
 let nameSong = document.querySelector('.name-song');
 nameSong.textContent = playList[playNum].title;
+
 let globalTimeToSeek = 0;
-let globalVolume = 0.5;
+let globalVolume = 0.3;
+
+const volume = document.querySelector('.volume');
+const audioPlayer = document.querySelector('.player')
+
+audioPlayer.querySelector(".volume-percentage").style.height = globalVolume * 100 + '%';
 
 function playAudio() {
   audio.src = playList[playNum].src;
@@ -295,9 +397,6 @@ divPlayTrack.forEach((el, index) => {
 
 audio.addEventListener('ended', playNext);
 
-const volume = document.querySelector('.volume');
-const audioPlayer = document.querySelector('.player')
-
 volume.addEventListener("click", () => {
   audio.muted = !audio.muted;
   if (audio.muted) {
@@ -353,4 +452,67 @@ volumeSlider.addEventListener('click', (e) => {
   audioPlayer.querySelector(".volume-percentage").style.height = newVolume * 100 + '%';
 });
 
+//------------------------------SETTINGS--------------------------------------------
+
+const settingsBtn = document.querySelector('.settings');
+const settingsBack = document.querySelector('.settings-bg');
+
+
+settingsBtn.addEventListener('click', () => {
+  settingsBack.classList.toggle('active');
+  settingsBtn.classList.toggle('active');
+})
+
+
+//------------------------------PHOTOS-from-API--------------------------------------------
+
+async function getLinkToImageFromUnsplash() {
+  let timeOfDay = globalActiveTag;
+  const url = `https://api.unsplash.com/photos/random?orientation=landscape&query=${timeOfDay}&client_id=dp0dFHpt-gWwEbAVwVYdt1FguTTWKvkC3sOcgmNMotM`;
+  const res = await fetch(url);
+  const data = await res.json();
+  let picUrl = data.urls.raw + "&w=1920";
+  console.log('Unsplash');
+  const img = new Image();
+  img.src = picUrl;
+  img.addEventListener('load', () => {
+    body.style.backgroundImage = `url(${picUrl})`;
+  });
+}
+
+async function getLinkToImageFromFlickr() {
+  let timeOfDay = globalActiveTag;
+  const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=af211cc952f928c1ef726f9c5111b3c7&tags=${timeOfDay}&orientation=landscape&width=1920&height=1080&extras=url_l&format=json&nojsoncallback=1`;
+  const res = await fetch(url);
+  const data = await res.json();
+  let picUrl = data.photos.photo[Math.floor(Math.random() * 90)].url_l
+  console.log('flickr');
+  const img = new Image();
+  img.src = picUrl;
+  img.addEventListener('load', () => {
+    body.style.backgroundImage = `url(${picUrl})`;
+  });
+}
+
+const select = document.getElementById('select')
+
+select.onchange = function () {
+  const activeTag = this.value;
+  globalActiveTag = activeTag;
+}
+
+arrOfPhotoBank.forEach((el) => {
+  el.addEventListener('click', () => {
+    arrOfPhotoBank.forEach((el) => {
+      el.classList.remove('active')
+    });
+    el.classList.toggle('active')
+    if (arrOfPhotoBank[0].classList.contains('active')) {
+      select.classList.remove('active')
+    }
+    else {
+      select.classList.add('active')
+    }
+  })
+});
 
